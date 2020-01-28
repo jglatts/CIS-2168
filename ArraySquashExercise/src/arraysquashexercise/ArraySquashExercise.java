30ebeb14b6ee7fbfc83b8604a7e8b64f6fce074b
John Glatts <john@z-axiscc.com>
2020-01-28 17:31:25 -0500

    Update ArraySquashExercise.java

--- ArraySquashExercise/src/arraysquashexercise/ArraySquashExercise.java
+++ ArraySquashExercise/src/arraysquashexercise/ArraySquashExercise.java
@@ -6,11 +6,9 @@ import java.util.Collections;
 
 /**
  * Somewhat working...
- * The last two array values are working w/ correct output
+ * The last three array values are working w/ correct output
+ * First one is causing trouble but it's getting better
  * 
- * In-order to swap the repeated values, must use Collections.frequency(dup_list, a[i]);
- *  - this will return amount of repeats, letting us know how many values of it we need
- *    in the new array 
  */
 
 public class ArraySquashExercise {
@@ -34,82 +32,26 @@ public class ArraySquashExercise {
   // THIS IS THE METHOD YOU NEED TO IMPLEMENT
   public static void squash(int[] a) {
     // YOUR CODE BELOW (PLEASE BE PREPARED TO EXPLAIN YOUR SOLUTION)
-    
-    /* Add a check that runs first to see if all elements are identical 
-       When repopulating the list, check if a value exists in the dup. list and reg. list
-    */
-    int size = a.length, count = 0, zero_idx;
-    ArrayList<Integer> list = findDuplicates(a);
-    int[] newArr = replaceDuplicates(list, a);
-    System.out.println("Duplicate List = " + list);
-    System.out.println("New Array = " + Arrays.toString(newArr));
-    // replace all elements from old-array w/ new-array 
-    for (int i = 0; i < a.length; ++i) {
-        a[i] = newArr[i];
-    }
+    newSquash(a);
   }
   
-  public static ArrayList<Integer> findDuplicates(int[] a) {
-      ArrayList<Integer> duplicate_list = new ArrayList<>();
-      for (int i = 1; i < a.length-1; ++i) {
-             if (a[i] == a[i+1]) {
-                duplicate_list.add(a[i]);
-                i++;
-             }
-      }
-      // we skipped the first element, so add it
-      if (duplicate_list.contains(a[0])) duplicate_list.add(0, a[0]);
-      return duplicate_list;
-  } 
-  
-  public static int[] replaceDuplicates(ArrayList<Integer> dup_list, int[] a) {
+  public static void newSquash(int[] a) {
       int[] newArr = new int[a.length];
-      if (dup_list.isEmpty()) {
-          for (int i = 0; i < a.length; ++i) {
+      for (int i = 0; i < a.length-1; i++) {
+          if (a[i] != a[i+1]) {
               newArr[i] = a[i];
+              newArr[i+1] = a[i+1];
+          } 
+          else {
+              newArr[i] = -1;
+              newArr[i+1] = a[i];
           }
-          return newArr;
       }
-      // remove un-neccs. else statement.. ant some point
-      else {
-            // now check if the list contains all the same elements 
-            int check_val = dup_list.get(0);
-            for (int i = 0; i < dup_list.size(); ++i) {
-                if (dup_list.get(i) != check_val) {
-                    break;  // exit this loop if elements aren't all the same    
-                }
-                // if we make it here, all elements are the same
-                if (i == (dup_list.size()-1)) {
-                    for (int x = 0; x < a.length; ++x) {
-                        if (x == 0) newArr[x] = check_val;
-                        else newArr[x] = -1;
-                        
-                    }
-                    return newArr;
-                }
-            }
-            
-            // this seems pretty good, get rid of hard-codes though
-            int check_limit = Collections.frequency(dup_list, 6);
-            int check_limit_two = Collections.frequency(dup_list, 3);
-            
-            // swap the ordering of the array
-            for (int i = 0; i < a.length; ++i) {
-               if (i == 0) newArr[i] = a[0];
-               else {
-                   if (dup_list.contains(a[i])) {
-                        if (i < dup_list.size()) newArr[i] = dup_list.get(i);
-                        else newArr[i] = -1;
-                   }
-                   else {
-                       newArr[i] = a[i];    // if the value is not in dup_list, add it to new list
-                   }
-               }
-            }    
+      for (int x = 0; x < a.length; ++x) {
+          a[x] = newArr[x];
       }
-      return newArr;
   }
-  
+    
   // DO NOT CHANGE THE stringInts() METHOD (IT'S USED BY TESTS IN main())
   /**
    *  stringInts() converts an array of ints to a String.
@@ -139,7 +81,8 @@ public class ArraySquashExercise {
 
     int[] test1 = {3, 7, 7, 7, 4, 5, 5, 2, 0, 8, 8, 8, 8, 5};
     System.out.println("squashing " + stringInts(test1) + ":");
-    squash(test1);
+    //squash(test1);
+    newSquash(test1);
     result = stringInts(test1);
     System.out.println(result);
     System.out.println("Should be: 3  7  4  5  2  0  8  5  -1  -1  -1  -1  -1  -1\n");
@@ -149,7 +92,8 @@ public class ArraySquashExercise {
 
     int[] test2 = {6, 6, 6, 6, 6, 3, 6, 3, 6, 3, 3, 3, 3, 3, 3};
     System.out.println("squashing " + stringInts(test2) + ":");
-    squash(test2);
+    //squash(test2);
+    newSquash(test2);
     result = stringInts(test2);
     System.out.println(result);
     System.out.println("Should be: 6  3  6  3  6  3  -1  -1  -1  -1  -1  -1  -1  -1  -1\n");
@@ -159,7 +103,8 @@ public class ArraySquashExercise {
 
     int[] test3 = {4, 4, 4, 4, 4};
     System.out.println("squashing " + stringInts(test3) + ":");
-    squash(test3);
+    //squash(test3);
+    newSquash(test3);
     result = stringInts(test3);
     System.out.println(result);
     System.out.println("Should be: 4  -1  -1  -1  -1\n");
@@ -168,7 +113,8 @@ public class ArraySquashExercise {
 
     int[] test4 = {0, 1, 2, 3, 4, 5, 6};
     System.out.println("squashing " + stringInts(test4) + ":");
-    squash(test4);
+    //squash(test4);
+    newSquash(test4);
     result = stringInts(test4);
     System.out.println(result);
     System.out.println("Should be: 0  1  2  3  4  5  6\n");
