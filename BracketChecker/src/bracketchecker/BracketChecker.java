96df2f73b9673c25cd60d8560cf7df64bc64e79a
John Glatts <john@z-axiscc.com>
2020-02-07 08:43:51 -0500

    Update BracketChecker.java

--- BracketChecker/src/bracketchecker/BracketChecker.java
+++ BracketChecker/src/bracketchecker/BracketChecker.java
@@ -30,8 +30,10 @@ class BracketChecker {
     public static void main(String[] args) throws IOException {
         InputStreamReader input_stream = new InputStreamReader(System.in);
         BufferedReader reader = new BufferedReader(input_stream);
+        Bracket opening_bracket = null;
+        Boolean check = true;
         String message = "";
-        System.out.print("Input: ");
+        System.out.print("Input: " );
         String text = reader.readLine();
         
         Stack<Bracket> opening_brackets_stack = new Stack<Bracket>();
@@ -44,20 +46,35 @@ class BracketChecker {
             }
             if (next == ')' || next == ']' || next == '}') {
                 // Process closing bracket, write your code here
-                Bracket opening_bracket = opening_brackets_stack.pop();
-                if (!opening_bracket.Match(next)) {
+                if (!opening_brackets_stack.isEmpty()) 
+                    opening_bracket = opening_brackets_stack.pop();
+                else {
+                    // Etack is empty, so no closing bracket
+                    // Exit loop and display position
                     message = "" + (position+1);
+                    break;                
+                }
+                if (!opening_bracket.Match(next)) {
+                    // Opening bracket doesn't equal closing bracket
+                    // Exit loop and display position
+                    message = "" + (position+1);
+                    check = false;
                     break;
                 } 
+                // If we made it here,
+                // both opening bracket and closing bracket match 
                 else message = "Success";  
             }
         }
-        if (!opening_brackets_stack.isEmpty()) message = "" + text.length();
+        // Check if there are any elements left in stack 
+        // If check == true, the opening bracket has no closing bracket
+        // Else , the opening bracket has a closing bracket, so don't display the message 
+        if (!opening_brackets_stack.isEmpty() && check) 
+            message = "" + (opening_brackets_stack.pop().position+1);
+        
         // Printing answer, write your code here
         System.out.println("\n" + message);
     }
-    
-
     
 }
     
