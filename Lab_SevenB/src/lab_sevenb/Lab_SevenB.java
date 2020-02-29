da1d134197c6f3b323b1e3fd8c918de1d83cdb8c
John Glatts <johnglatts1@hotmail.com>
2020-02-29 07:30:36 -0500

    Add files via upload

---
+++ Lab_SevenB/src/lab_sevenb/Lab_SevenB.java
@@ -0,0 +1,13 @@
+package lab_sevenb;
+
+public class Lab_SevenB {
+
+    public static void main(String[] args) {
+        MaxPQ.main(args);
+        System.out.println();
+        MinPQ.main(args);
+        System.out.println();
+        TopMints.main(args);
+    }
+    
+}
