c9048ba6debe1172a44acb13523397b2366d99b3
John Glatts <john@z-axiscc.com>
2020-04-24 20:34:58 -0400

    clean up

--- Lab12/src/lab12/Dijkstra.java
+++ Lab12/src/lab12/Dijkstra.java
@@ -5,41 +5,42 @@ import java.util.*;
 public class Dijkstra {
     
     public static PriorityQueue<Integer> queue = new PriorityQueue<>();
-    public static Set<Integer> settled = new HashSet<>();
+    public static Set<Integer> marked = new HashSet<>();
     public static int[] dist;
         
     private static int distance(ArrayList<Integer>[] adj, ArrayList<Integer>[] cost, int s, int t) {
         dist = new int[adj.length];
-        queue.add(s);
-
-        for (int i = 0; i < cost.length; ++i) dist[i] = Integer.MAX_VALUE;
-        dist[s] = 0;
+        queue.add(s);  // add source vertex to queue
         
-        while (settled.size() != adj.length-1) {
-            if (!queue.isEmpty()){
-                settled.add(queue.peek());
-                checkNeighbors(adj, cost, queue.poll());    
-            }
-            else return -1;
+        // set all distanaces to -1  
+        for (int i = 0; i < cost.length; ++i) 
+            dist[i] = Integer.MAX_VALUE; 
+        dist[s] = 0; // distance to source is 0
+        
+        // loop until we've visited all neighboring vertices
+        while (!queue.isEmpty()) {
+                int v = queue.poll();
+                marked.add(v);
+                checkNeighbors(adj, cost, v);    
         }
         return dist[t];
     }
 
     private static void checkNeighbors(ArrayList<Integer>[] adj, ArrayList<Integer>[] cost, int v) {
+        // loop through adjacent vertices
         for (int i = 0; i < adj[v].size(); i++) { 
             int x = adj[v].get(i); 
-            if (!settled.contains(x)) { 
-                int edgeDist = cost[v].get(i); 
-                int newDist = dist[v] + edgeDist; 
-                if (newDist < dist[x]) 
-                    dist[x] = newDist; 
+            if (!marked.contains(x)) { 
+                int edgeDist = cost[v].get(i);    // get the current cost from node v to to adjacent node  
+                int newDist = dist[v] + edgeDist; // calculate new cost
+                if (newDist < dist[x]) dist[x] = newDist; 
                 queue.add(x); 
             } 
         }       
     }
     
     public static void main(String[] args) {
-        In in = new In("G4-1.txt");
+        In in = new In("G4-2.txt");
         int n = in.readInt();
         int m = in.readInt();
         ArrayList<Integer>[] adj  = new ArrayList[n];
